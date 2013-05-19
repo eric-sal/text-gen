956426a85412aa4f078cecec13312d89817b9e84
Eric Salczynski <eric@wehaventthetime.com>
2013-05-18 23:02:52 -0400

    Update TextGen.cs
    Added copyright info

--- Editor/TextGen.cs
+++ Editor/TextGen.cs
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 Eric Salczynski and Ramón Rocha
+// This program (Text Gen) is released under the MIT License.
+// http://opensource.org/licenses/MIT
+
 #if UNITY_EDITOR
 
 using UnityEditor;
